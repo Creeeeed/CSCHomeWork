6559749266dee7a8a16c08a9e2eddb657788ce0a
Creeeeed <89224604+Creeeeed@users.noreply.github.com>
2022-08-28 11:18:23 -0600

    Yes

--- src/csc251/util/ArrayUtil.java
+++ src/csc251/util/ArrayUtil.java
@@ -8,10 +8,13 @@ public class ArrayUtil{
     public static int[] copyOfRange(int[] original, int from, int to){
 
     }
-    public static int sum(int[] appends){
-
+    public static int sum(int[] addends) {
+        int result = 0;
+        for (int addend : addends)
+            result += addend;
+        return result;
     }
-    public static int sum(int[][] appends){
+    public static int sum(int[][] addends){
 
     }
 
---
+++ src/csc251/util/UseBMICalculator.java
@@ -0,0 +1,13 @@
+package csc251.util;
+
+public class UseBMICalculator {
+    private String name;
+
+    private int age;
+
+    private double weight;
+
+    private double height;
+
+}
+

abc552804cecd0ec3bb7fdbba797a3a050d12f0a
Creeeeed <89224604+Creeeeed@users.noreply.github.com>
2022-08-28 07:42:44 -0600

    4 methods

--- src/csc251/util/ArrayUtil.java
+++ src/csc251/util/ArrayUtil.java
@@ -2,5 +2,18 @@ package csc251.util;
 
 public class ArrayUtil{
 
+    public static void fill(int[] toFill, int val) {
+
+    }
+    public static int[] copyOfRange(int[] original, int from, int to){
+
+    }
+    public static int sum(int[] appends){
+
+    }
+    public static int sum(int[][] appends){
+
+    }
+
 
 }

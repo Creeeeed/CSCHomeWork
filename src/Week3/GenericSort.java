8dd73a1c51c05a66a326b69fd738160d655802c7
Creeeeed <89224604+Creeeeed@users.noreply.github.com>
2022-09-04 19:49:21 -0600

    Yes

--- src/Week3/GenericSort.java
+++ src/Week3/GenericSort.java
@@ -1,4 +1,59 @@
 package Week3;
 
 public class GenericSort {
+    private Integer[] intArray;
+
+    private Double[] doubleArray;
+
+    private Character[] charArray;
+
+    private String[] stringArray;
+
+    public GenericSort(Integer[] intArray, Double[] doubleArray, Character[] charArray, String... stringArray){
+
+    }
+
+
+
+    public Integer[] getIntArray() {
+        return intArray;
+    }
+
+    public void setIntArray(Integer[] intArray) {
+        this.intArray = intArray;
+    }
+
+    public Double[] getDoubleArray() {
+        return doubleArray;
+    }
+
+    public void setDoubleArray(Double[] doubleArray) {
+        this.doubleArray = doubleArray;
+    }
+
+    public Character[] getCharArray() {
+        return charArray;
+    }
+
+    public void setCharArray(Character[] charArray) {
+        this.charArray = charArray;
+    }
+
+    public String[] getStringArray() {
+        return stringArray;
+    }
+
+    public void setStringArray(String[] stringArray) {
+        this.stringArray = stringArray;
+    }
+
+    public void printList(Object[] list) {
+
+    }
+    public void sort(Comparable[] list){
+
+
+    }
+
+
 }
--- src/csc251/util/ArrayUtil.java
+++ src/csc251/util/ArrayUtil.java
@@ -1,47 +1,14 @@
 package csc251.util;
- // correct fill method
-public class ArrayUtil{
-    public static void main(String[] args) {
-        ArrayUtil array = new ArrayUtil();
-        array.fill;
 
-    }
-
+// correct fill method
+public class ArrayUtil {
     public static void fill(int[] toFill, int val) {
-        //numbers go in here
+     for (int i = 0; toFill.length > i; i++) {
+      toFill[i] = val;
 
-    }
+     }
 
-    public static int[] copyOfRange(int[] original, int from, int to) {
-        return new int[0];
     }
 
 }
-    public static int sum(int[] addends) {
-        int result = 0;
-        for (int addend : addends)
-            result += addend;
-        return result;
-    }
-    public static int sum(int[][] addends){
 
-
-    }
-
-
-}
-
-
-
-
-
-
-// Fill should place the passed in value into each cell within the array
-// leaving the array filled entirely with the value.
-//opyOfRange should copy the portion of the array
-// specified by the from and to into a new array
-// of the same type
-// and return the new array.
-//Sum is overloaded with two implementations.
-// One that sums an array of ints.
-// The second should sum an array of arrays of ints.
